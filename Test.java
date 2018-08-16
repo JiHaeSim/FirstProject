4790bb66804cbd5d89f52b0e40002f7768593b8a
jihae.sim <simlucy0129@naver.com>
2018-08-16 13:06:13 +0900

    branch3추가

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ class Test{
 	public static void main(String[] args){
 		system.out.println("Heo Git");
 		system.out.println("Hi Git");
-		system.out.println("branch1");
+		system.out.println("branch3");
 	}
 }
